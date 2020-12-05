0fb915e88fea8517d460cc1dae7eceb8aabf72ea
Arjun Tripathy <arjunt2@illinois.edu>
2020-12-04 21:19:17 -0800

    Full functional cmd line interface w stalemates

--- chess.py
+++ chess.py
@@ -6,42 +6,49 @@ b.print_board()
 
 white_move = 1
 while True:
-  prompt = "Enter move for " + "White: "*white_move + "Black: "*(~white_move)
+    prompt = "Enter move for " + "White: "*white_move + "Black: "*(~white_move)
 
-  move = input(prompt).strip()
-  if move == "quit":
-    break
+    move = input(prompt).strip()
+    if move == "quit":
+        break
 
-  color = "white"*white_move + "black"*(~white_move) 
+    color = "white"*white_move + "black"*(~white_move)
 
-  err = None
-  if len(move.split(" ")) != 3:
-    if "O-O-O" in move:
-      err = b.castle(color, 0)
-    elif "O-O" in move:
-      err = b.castle(color, 7)
+    err = None
+    if len(move.split(" ")) != 3:
+        if "O-O-O" in move:
+            err = b.castle(color, 0)
+        elif "O-O" in move:
+            err = b.castle(color, 7)
+        else:
+            print("Illegal input")
+            continue
     else:
-      print("Illegal input")
-      continue 
-  else: 
-    type = move.split(" ")[0]
-    start = move.split(" ")[1]
-    dest = move.split(" ")[2]
-    
-    err = b.move(color, type, start, dest)
+        type = move.split(" ")[0]
+        start = move.split(" ")[1]
+        dest = move.split(" ")[2]
 
-  if err != None and err != "Check":
-    print(err)
-    continue
+        err = b.move(color, type, start, dest)
+
+    if err != None and err != "Check":
+        print(err)
+        continue
+
+    b.print_board()
     
-  b.print_board()
-  if err == "Check":
     copy_board = deepcopy(b)
-    if not copy_board.can_escape_check("white"*(~white_move) + "black"*white_move):
-      print("Checkmate! " + "White"*white_move + "Black"*(~white_move) + " wins!")
-      break
-    print("Check!")
+    if not copy_board.can_move("white"*(~white_move) + "black"*white_move):
+        if err == "Check":
+            print("Checkmate! " + "White"*white_move + "Black"*(~white_move) + " wins!")
+        else:
+            print("Draw by stalemate.")
+        break
 
-  
-  white_move = ~white_move
-  
+    if err == "Check":
+        print("Check!")
+
+
+
+
+
+    white_move = ~white_move
--- pieces.py
+++ pieces.py
@@ -1,283 +1,205 @@
+def print_moves(valid_squares):
+    print("Moves: ", end="")
+    for s in valid_squares:
+        print(chr(s[0] + 97) + str(s[1] + 1), end = ", ")
+    print("")
+
+
 class piece:
-  def __init__(self, type, color, pos):
-    self.type = type
-    self.color = color
-    self.pos = pos
+    def __init__(self, type, color, pos):
+        self.type = type
+        self.color = color
+        self.pos = pos
 
-  def print_piece(self):
-    if self.color == "black":
-      print("\033[4m" + self.type + "\033[0m", end = " ")
-    else:
-      print(self.type, end = " ")
-  
-  def get_x(self):
-    return self.pos[0]
-  def get_y(self):
-    return self.pos[1]
-  def get_color(self):
-    return self.color
-  def get_type(self):
-    return self.type
-  
-  def set_pos(self, x, y):
-    self.pos = (x, y)
+    def print_piece(self):
+        if self.color == "black":
+            print("\033[4m" + self.type + "\033[0m", end = " ")
+        else:
+            print(self.type, end = " ")
 
-  def check_square(self, graph, x, y):
-    return graph[x][y].color == "empty"
+    def get_x(self):
+        return self.pos[0]
+    def get_y(self):
+        return self.pos[1]
+    def get_color(self):
+        return self.color
+    def get_type(self):
+        return self.type
 
-      
+    def set_pos(self, x, y):
+        self.pos = (x, y)
 
-class empty(piece):
-  def __init__(self, pos):
-    piece.__init__(self, " ", "empty", pos)
+    def check_square(self, graph, x, y):
+        return graph[x][y].color == "empty"
 
-class pawn(piece):
-  def __init__(self, color, pos):
-    piece.__init__(self, "p", color, pos)
-  
-  def check_valid_path(self, graph, dest_x, dest_y):
-    direction = (self.color == "white") - (self.color == "black")
-    if dest_x == self.pos[0]:
-      if dest_y == self.pos[1] + direction:
-        return True
-      if dest_y == self.pos[1] + 2*direction:
-        if dest_y != 3 + (self.color == "black"):
-          return False
-        return self.check_square(graph, dest_x, dest_y - direction)
-      else:
-        return False 
-    if dest_y == self.pos[1] + direction:
-      if dest_x == self.pos[0] - 1 or dest_x == self.pos[0] + 1:
-        return graph[dest_x][dest_y].get_color() != self.color and graph[dest_x][dest_y].get_color() != "empty"
-    return False
+    def check_straights(self, graph):
+        valid_squares = set()
+        # check vertical up
+        for y in range(self.pos[1] + 1, 8):
+            valid_squares.add((self.pos[0], y))
+            if graph[self.pos[0]][y].get_color() != "empty":
+                break
+        # check vertical down
+        for y in range(self.pos[1] - 1, -1, -1):
+            valid_squares.add((self.pos[0], y))
+            if graph[self.pos[0]][y].get_color() != "empty":
+                break
+        # check horizontal right
+        for x in range(self.pos[0] + 1, 8):
+            valid_squares.add((x, self.pos[1]))
+            if graph[x][self.pos[1]].get_color() != "empty":
+                break
+        # check horizontal left
+        for x in range(self.pos[0] - 1, -1, -1):
+            valid_squares.add((x, self.pos[1]))
+            if graph[x][self.pos[1]].get_color() != "empty":
+                break
 
+        return valid_squares
 
-class rook(piece):
-  def __init__(self, color, pos):
-    piece.__init__(self, "r", color, pos)
-    self.can_castle = True
-  
-  def toggle_castle_flag(self):
-    self.can_castle = False
+    def check_diagonals(self, graph):
+        valid_squares = set()
+        # check diagonal upper right
+        for k in range(1, 1 + min(7 - self.pos[0], 7 - self.pos[1])):
+            valid_squares.add((self.pos[0] + k, self.pos[1] + k))
+            if graph[self.pos[0] + k][self.pos[1] + k].get_color() != "empty":
+                break
+        # check diagonal upper left
+        for k in range(1, 1 + min(self.pos[0], 7 - self.pos[1])):
+            valid_squares.add((self.pos[0] - k, self.pos[1] + k))
+            if graph[self.pos[0] - k][self.pos[1] + k].get_color() != "empty":
+                break
+        # check diagonal lower right
+        for k in range(1, 1 + min(7 - self.pos[0], self.pos[1])):
+            valid_squares.add((self.pos[0] + k, self.pos[1] - k))
+            if graph[self.pos[0] + k][self.pos[1] - k].get_color() != "empty":
+                break
+        # check diagonal lower left
+        for k in range(1, 1 + min(self.pos[0], self.pos[1])):
+            valid_squares.add((self.pos[0] - k, self.pos[1] - k))
+            if graph[self.pos[0] - k][self.pos[1] - k].get_color() != "empty":
+                break
 
-  def get_castle_flag(self):
-    return self.can_castle
+        return valid_squares
 
-  def check_valid_path(self, graph, dest_x, dest_y):
-    if dest_x == self.pos[0] and dest_y != self.pos[1]:
-      direction = (self.pos[1] < dest_y) - (dest_y < self.pos[1])
-      for y in range(self.pos[1] + direction, dest_y, direction):
-        if not self.check_square(graph, dest_x, y):
-          return False
-      return True  
+    def check_knights(self, graph):
+        valid_squares = set()
+        for p in [(2,1), (1,2), (-2,1), (-1,2), (2,-1), (1,-2), (-2,-1), (-1,-2)]:
+            if self.pos[0] + p[0] in range(8) and self.pos[1] + p[1] in range(8):
+                valid_squares.add((self.pos[0] + p[0], self.pos[1] + p[1]))
 
-    if dest_y == self.pos[1] and dest_x != self.pos[0]:
-      direction = (self.pos[0] < dest_x) - (dest_x < self.pos[0])
-      for x in range(self.pos[0] + direction, dest_x, direction):
-        if not self.check_square(graph, x, dest_y):
-          return False
-      return True  
-    
-    return False
+        return valid_squares
 
+    def check_kings(self, graph):
+        valid_squares = set()
+        for p in [(-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0)]:
+            if self.pos[0] + p[0] in range(8) and self.pos[1] + p[1] in range(8):
+                valid_squares.add((self.pos[0] + p[0], self.pos[1] + p[1]))
 
-class knight(piece):
-  def __init__(self, color, pos):
-    piece.__init__(self, "n", color, pos)
+        return valid_squares
 
-  def check_valid_path(self, graph, dest_x, dest_y):
-    if abs(dest_x - self.pos[0]) == 2:
-      if abs(dest_y - self.pos[1]) == 1:
-        return True
-    if abs(dest_y - self.pos[1]) == 2:
-      if abs(dest_x - self.pos[0]) == 1:
-        return True
-    
-    return False
+    def check_pawns(self, graph):
+        valid_squares = set()
+        direction = (self.color == "white") - (self.color == "black")
+        # check double move
+        if self.pos[1] == 1 + (self.color == "black")*5:
+            if self.check_square(graph, self.pos[0], self.pos[1] + 2*direction):
+                valid_squares.add((self.pos[0], self.pos[1] + 2*direction))
+        # check double straight
+        if self.check_square(graph, self.pos[0], self.pos[1] + direction):
+            valid_squares.add((self.pos[0], self.pos[1] + direction))
+        # check attack
+        for k in [-1,1]:
+            if self.pos[0] + k in range(8):
+                if graph[self.pos[0] + k][self.pos[1] + direction].get_color() not in [self.color, "empty"]:
+                    valid_squares.add((self.pos[0] + k, self.pos[1] + direction))
 
-class bishop(piece):
-  def __init__(self, color, pos):
-    piece.__init__(self, "b", color, pos)
+        return valid_squares
 
-  def check_valid_path(self, graph, dest_x, dest_y):
-    if dest_x - self.pos[0] == dest_y - self.pos[1]:
-      if dest_x - self.pos[0] > 0:
-        for k in range(1, dest_x - self.pos[0]):
-          if not self.check_square(graph, self.pos[0] + k, self.pos[1] + k):
-            return False
-        return True
-      if dest_x - self.pos[0] < 0:
-        for k in range(1, self.pos[0] - dest_x):
-          if not self.check_square(graph, self.pos[0] - k, self.pos[1] - k):
-            return False
-        return True
-      return False
-
-    if dest_x - self.pos[0] == self.pos[1] - dest_y:
-      if dest_x - self.pos[0] > 0:
-        for k in range(1, dest_x - self.pos[0]):
-          if not self.check_square(graph, self.pos[0] + k, self.pos[1] - k):
-            return False
-        return True
-      if dest_x - self.pos[0] < 0:
-        for k in range(1, self.pos[0] - dest_x):
-          if not self.check_square(graph, self.pos[0] - k, self.pos[1] + k):
-            return False
-        return True
-
-    return False
-
-
-class queen(piece):
-  def __init__(self, color, pos):
-    piece.__init__(self, "Q", color, pos)
-
-  def check_valid_path(self, graph, dest_x, dest_y):
-    if dest_x == self.pos[0] and dest_y != self.pos[1]:
-      direction = (self.pos[1] < dest_y) - (dest_y < self.pos[1])
-      for y in range(self.pos[1] + direction, dest_y, direction):
-        if not self.check_square(graph, dest_x, y):
-          return False
-      return True  
-
-    if dest_y == self.pos[1] and dest_x != self.pos[0]:
-      direction = (self.pos[0] < dest_x) - (dest_x < self.pos[0])
-      for x in range(self.pos[0] + direction, dest_x, direction):
-        if not self.check_square(graph, x, dest_y):
-          return False
-      return True  
-  
-    if dest_x - self.pos[0] == dest_y - self.pos[1]:
-      if dest_x - self.pos[0] > 0:
-        for k in range(1, dest_x - self.pos[0]):
-          if not self.check_square(graph, self.pos[0] + k, self.pos[1] + k):
-            return False
-        return True
-      if dest_x - self.pos[0] < 0:
-        for k in range(1, self.pos[0] - dest_x):
-          if not self.check_square(graph, self.pos[0] - k, self.pos[1] - k):
-            return False
-        return True
-      return False
-
-    if dest_x - self.pos[0] == self.pos[1] - dest_y:
-      if dest_x - self.pos[0] > 0: 
-        for k in range(1, dest_x - self.pos[0]):
-          if not self.check_square(graph, self.pos[0] + k, self.pos[1] - k):
-            return False
-        return True
-      if dest_x - self.pos[0] < 0:
-        for k in range(1, self.pos[0] - dest_x):
-          if not self.check_square(graph, self.pos[0] - k, self.pos[1] + k):
-            return False
-        return True
-
-    return False
-
-
-class king(piece):
-  def __init__(self, color, pos):
-    piece.__init__(self, "K", color, pos)
-    self.can_castle = True
-
-  def toggle_castle_flag(self):
-    self.can_castle = False
-
-  def check_valid_path(self, graph, dest_x, dest_y):
-    if dest_x == self.pos[0] and dest_y == self.pos[1]:
-      return False
-    if abs(dest_x - self.pos[0]) <= 1:
-      if abs(dest_y - self.pos[1]) <= 1:
-        return True
-
-    return False
-
-  def check_castle(self, graph, rook_x):
-    if not self.can_castle or not graph[rook_x][self.pos[1]].get_castle_flag():
-      return "Cannot castle after moving King or rook"
-    direction = (self.pos[0] > rook_x) - (rook_x > self.pos[0])
-    for i in range(2):
-      if self.check_check(graph, self.pos[0] + i*direction, self.pos[1]):
-        return "Cannot castle through check"
-    if rook_x == 0:
-       if not self.check_square(graph, 1, self.pos[1]):
-        return "Illegal move"     
-
-  def check_check(self, graph, king_x = None, king_y = None):
-    if not king_x:
-      king_x = self.pos[0]
-    if not king_y:
-      king_y = self.pos[1]
-
-    # check vertical up
-    for y in range(king_y + 1, 8):
-      if graph[king_x][y].get_color() != self.color and graph[king_x][y].get_type() in ["r", "Q"]:
-        return True
-      if graph[king_x][y].get_color() != "empty":
-        break
-    # check vertical down
-    for y in range(king_y - 1, -1, -1):
-      if graph[king_x][y].get_color() != self.color and graph[king_x][y].get_type() in ["r", "Q"]:
-        return True    
-      if graph[king_x][y].get_color() != "empty":
-        break
-
-    # check horizontal right
-    for x in range(king_x + 1, 8):
-      if graph[x][king_y].get_color() != self.color and graph[x][king_y].get_type() in ["r", "Q"]:
-        return True
-      if graph[x][king_y].get_color() != "empty":
-        break
-    # check horizontal left
-    for x in range(king_x - 1, -1, -1):
-      if graph[x][king_y].get_color() != self.color and graph[x][king_y].get_type() in ["r", "Q"]:
-        return True
-      if graph[x][king_y].get_color() != "empty":
-        break
-
-    # check diagonal upper right
-    for k in range(1, min(8 - king_x, 8 - king_y)):
-      if graph[king_x + k][king_y + k].get_color() != self.color and graph[king_x + k][king_y + k].get_type() in ["b", "Q"]:
-        return True
-      if graph[king_x + k][king_y + k].get_color() != self.color and graph[king_x + k][king_y + k].get_type() == "p" and self.color == "white":
-        if k == 1:
-          return True
-      if graph[king_x + k][king_y + k].get_color() != "empty":
-        break
-    # check diagonal upper left
-    for k in range(1, min(king_x, 8 - king_y)):
-      if graph[king_x - k][king_y + k].get_color() != self.color and graph[king_x - k][king_y + k].get_type() in ["b", "Q"]:
-        return True
-      if graph[king_x - k][king_y + k].get_color() != self.color and graph[king_x - k][king_y + k].get_type() == "p" and self.color == "white":
-        if k == 1:
-          return True
-      if graph[king_x - k][king_y + k].get_color() != "empty":
-        break
-    # check diagonal lower right
-    for k in range(1, min(8 - king_x, king_y)):
-      if graph[king_x + k][king_y - k].get_color() != self.color and graph[king_x + k][king_y - k].get_type() in ["b", "Q"]:
-        return True
-      if graph[king_x + k][king_y - k].get_color() != self.color and graph[king_x + k][king_y - k].get_type() == "p":
-        if k == 1:
-          return True
-      if graph[king_x + k][king_y - k].get_color() != "empty":
-        break
-    # check diagonal lower left
-    for k in range(1, min(king_x, king_y)):
-      if graph[king_x - k][king_y - k].get_color() != self.color and graph[king_x - k][king_y - k].get_type() in ["b", "Q"]:
-        return True
-      if graph[king_x - k][king_y - k].get_color() != self.color and graph[king_x - k][king_y - k].get_type() == "p":
-        if k == 1:
-          return True
-      if graph[king_x - k][king_y - k].get_color() != "empty":
-        break
-    
-    # check for knights
-    for p in [(2,1), (1,2), (-2,1), (-1,2), (2,-1), (1,-2), (-2,-1), (-1,-2)]:
-      if king_x + p[0] in range(0,8) and king_y + p[1] in range(0,8):
-        if graph[king_x + p[0]][king_y + p[1]].get_type() == "n":
-          if graph[king_x + p[0]][king_y + p[1]].get_color() != self.color:
+    def check_attackers(self, graph):
+        attackers = {s for s in self.check_diagonals(graph) if graph[s[0]][s[1]].get_color() != self.color and graph[s[0]][s[1]].get_type() in ["b", "Q"]}
+        if len(attackers) > 0:
+            return True
+        attackers = {s for s in self.check_straights(graph) if graph[s[0]][s[1]].get_color() != self.color and graph[s[0]][s[1]].get_type() in ["r", "Q"]}
+        if len(attackers) > 0:
+            return True
+        attackers = {s for s in self.check_knights(graph) if graph[s[0]][s[1]].get_color() != self.color and graph[s[0]][s[1]].get_type() in ["n"]}
+        if len(attackers) > 0:
+            return True
+        attackers = {s for s in self.check_pawns(graph) if graph[s[0]][s[1]].get_color() != self.color and graph[s[0]][s[1]].get_type() in ["p"]}
+        if len(attackers) > 0:
+            return True
+        attackers = {s for s in self.check_kings(graph) if graph[s[0]][s[1]].get_color() != self.color and graph[s[0]][s[1]].get_type() in ["K"]}
+        if len(attackers) > 0:
             return True
 
-    return False
-      
+        return False
+
+
+class empty(piece):
+    def __init__(self, pos):
+        piece.__init__(self, " ", "empty", pos)
+
+class pawn(piece):
+    def __init__(self, color, pos):
+        piece.__init__(self, "p", color, pos)
+
+    def get_valid_squares(self, graph):
+        return {s for s in self.check_pawns(graph) if graph[s[0]][s[1]].get_color() != self.color}
+
+class rook(piece):
+    def __init__(self, color, pos):
+        piece.__init__(self, "r", color, pos)
+        self.can_castle = True
+
+    def toggle_castle_flag(self):
+        self.can_castle = False
+
+    def get_castle_flag(self):
+        return self.can_castle
+
+    def get_valid_squares(self, graph):
+        return {s for s in self.check_straights(graph) if graph[s[0]][s[1]].get_color() != self.color}
+
+class knight(piece):
+    def __init__(self, color, pos):
+        piece.__init__(self, "n", color, pos)
+
+    def get_valid_squares(self, graph):
+        return {s for s in self.check_knights(graph) if graph[s[0]][s[1]].get_color() != self.color}
+
+class bishop(piece):
+    def __init__(self, color, pos):
+        piece.__init__(self, "b", color, pos)
+
+    def get_valid_squares(self, graph):
+        return {s for s in self.check_diagonals(graph) if graph[s[0]][s[1]].get_color() != self.color}
+
+class queen(piece):
+    def __init__(self, color, pos):
+        piece.__init__(self, "Q", color, pos)
+
+    def get_valid_squares(self, graph):
+        return {s for s in self.check_straights(graph).union(self.check_diagonals(graph)) if graph[s[0]][s[1]].get_color() != self.color}
+
+class king(piece):
+    def __init__(self, color, pos):
+        piece.__init__(self, "K", color, pos)
+        self.can_castle = True
+
+    def toggle_castle_flag(self):
+        self.can_castle = False
+
+    def get_valid_squares(self, graph):
+        return {s for s in self.check_kings(graph) if graph[s[0]][s[1]].get_color() != self.color}
+
+    def check_castle(self, graph, rook_x):
+        if not self.can_castle or not graph[rook_x][self.pos[1]].get_castle_flag():
+            return "Cannot castle after moving King or rook"
+        direction = (self.pos[0] > rook_x) - (rook_x > self.pos[0])
+        for i in range(2):
+            if graph[self.pos[0] + i*direction][self.pos[1]].check_attackers(graph):
+                return "Cannot castle through check"
+        if rook_x == 0:
+             if not self.check_square(graph, 1, self.pos[1]):
+                return "Illegal move"
